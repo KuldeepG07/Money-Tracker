52a2281e78cbbc081b07314a5f893039085580f7
Kuldeep Gabani <k4gabani@gmail.com>
2024-08-27 15:41:10 +0530

    Dtabase console check

--- Node_Backend/configurations/database.js
+++ Node_Backend/configurations/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/Flutter_MoneyTracker').on('open', ()=> {
-    console.log('MongoDB Database connected...');
-});
+    console.log('Database connected...');
+}); 
 
 module.exports = connection;
