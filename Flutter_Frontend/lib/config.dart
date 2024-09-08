8dfda701ca0da3795dcbcee8a6d41d4a0b0adc46
Kuldeep Gabani <k4gabani@gmail.com>
2024-09-08 23:23:46 +0530

    delete frontend code

--- Flutter_Frontend/lib/config.dart
+++
@@ -1,6 +0,0 @@
-final url = 'http://127.0.0.1:3000/';
-final loginUrl = 'http://127.0.0.1:3000/login';
-final signupUrl = url + 'signup';
-final getUserByEmail = url + 'getuser';
-final getRecentExpenses = url + 'getrecentexpenses';
-final getRecentIncomes = url + 'getrecentincomes';
